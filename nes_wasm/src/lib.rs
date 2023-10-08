b81265d54394b0162dd9bf1dfed93c3f6e32420e
tomoesaturn <hotarutwitter@gmail.com>
2023-10-08 23:40:40 +0900

    segregate keyboard event handler from rs

--- nes_wasm/src/lib.rs
+++ nes_wasm/src/lib.rs
@@ -1,34 +1,22 @@
 mod adapter_impl;
-mod util;
 
 use std::{cell::RefCell, rc::Rc};
 
 use adapter_impl::{audio::AudioCtx, cartridge::CartridgeCtx, video::VideoCtx};
 use js_sys::Uint8Array;
 use nes_core::{adapter::nes::NesAdapter, usecase::nes::NesState};
-use util::{run_request_animation_frame_loop, FrameSuccesor};
 use wasm_bindgen::prelude::*;
-use web_sys::{window, CanvasRenderingContext2d, HtmlCanvasElement, KeyboardEvent};
+use web_sys::{window, CanvasRenderingContext2d, HtmlCanvasElement};
 
 #[wasm_bindgen]
-pub fn start_nes(canvas_id: &str, nes_file: Uint8Array) {
-    WindowContext::default().init_nes(canvas_id, nes_file);
+pub struct WindowContext {
+    nes_state: Rc<RefCell<NesState>>,
 }
 
-#[derive(Default)]
-struct WindowContext {
-    nes_state: Option<Rc<RefCell<NesState>>>,
-    frame_closure: Option<Rc<RefCell<Option<Closure<dyn FnMut()>>>>>,
-}
-
-impl FrameSuccesor for NesState {
-    fn run_one_frame(&mut self) {
-        self.run_frame();
-    }
-}
-
+#[wasm_bindgen]
 impl WindowContext {
-    fn init_nes(&mut self, canvas_id: &str, nes_file: Uint8Array) {
+    #[wasm_bindgen(constructor)]
+    pub fn new(canvas_id: &str, nes_file: Uint8Array) -> WindowContext {
         let nes_state = Rc::new(RefCell::new(
             NesAdapter {
                 cartridge: Box::new(CartridgeCtx {
@@ -54,72 +42,124 @@ impl WindowContext {
             }
             .init(),
         ));
-        self.nes_state = Some(nes_state.clone());
-        Self::add_keyboard_listener(nes_state.clone());
-        self.frame_closure = Some(run_request_animation_frame_loop(nes_state));
+        Self::run_request_animation_frame_loop(nes_state.clone());
+
+        WindowContext {
+            nes_state: nes_state,
+        }
     }
 
-    fn add_keyboard_listener(nes_state_ref: Rc<RefCell<NesState>>) {
-        let nes_state_ref1 = nes_state_ref.clone();
-        let keydown_listener =
-            Closure::<dyn Fn(KeyboardEvent)>::wrap(Box::new(move |event: KeyboardEvent| {
-                let code = event.key_code();
-                let mut nes_state = nes_state_ref1.borrow_mut();
-                match code {
-                    // X
-                    88 => nes_state.joypad.state_1p.A = true,
-                    // Z
-                    90 => nes_state.joypad.state_1p.B = true,
-                    // A
-                    65 => nes_state.joypad.state_1p.SELECT = true,
-                    // S
-                    83 => nes_state.joypad.state_1p.START = true,
-                    // Right
-                    39 => nes_state.joypad.state_1p.RIGHT = true,
-                    // Left
-                    37 => nes_state.joypad.state_1p.LEFT = true,
-                    // Down
-                    40 => nes_state.joypad.state_1p.DOWN = true,
-                    // Up
-                    38 => nes_state.joypad.state_1p.UP = true,
-                    _ => {}
-                }
-            }));
-        window()
-            .unwrap()
-            .add_event_listener_with_callback("keydown", keydown_listener.as_ref().unchecked_ref())
-            .unwrap();
-        keydown_listener.forget();
+    fn run_request_animation_frame_loop(nes_state: Rc<RefCell<NesState>>) {
+        // We use Rc<RefCell<None>> trick for recursive calling of request_animation_frame.
+        let f = Rc::new(RefCell::new(None));
+        let g = f.clone();
+        *g.borrow_mut() = Some(Closure::new(move || {
+            nes_state.as_ref().borrow_mut().run_frame();
+            Self::request_animation_frame(f.borrow().as_ref().unwrap());
+        }));
+        Self::request_animation_frame(g.borrow().as_ref().unwrap());
+    }
 
-        let nes_state_ref2 = nes_state_ref.clone();
-        let keyup_listener =
-            Closure::<dyn Fn(KeyboardEvent)>::wrap(Box::new(move |event: KeyboardEvent| {
-                let code = event.key_code();
-                let mut nes_state = nes_state_ref2.borrow_mut();
-                match code {
-                    // X
-                    88 => nes_state.joypad.state_1p.A = false,
-                    // Z
-                    90 => nes_state.joypad.state_1p.B = false,
-                    // A
-                    65 => nes_state.joypad.state_1p.SELECT = false,
-                    // S
-                    83 => nes_state.joypad.state_1p.START = false,
-                    // Right
-                    39 => nes_state.joypad.state_1p.RIGHT = false,
-                    // Left
-                    37 => nes_state.joypad.state_1p.LEFT = false,
-                    // Down
-                    40 => nes_state.joypad.state_1p.DOWN = false,
-                    // Up
-                    38 => nes_state.joypad.state_1p.UP = false,
-                    _ => {}
-                }
-            }));
+    fn request_animation_frame(f: &Closure<dyn FnMut()>) {
         window()
             .unwrap()
-            .add_event_listener_with_callback("keyup", keyup_listener.as_ref().unchecked_ref())
+            .request_animation_frame(f.as_ref().unchecked_ref())
             .unwrap();
-        keyup_listener.forget();
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keydown_A(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.A = true;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keydown_B(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.B = true;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keydown_SELECT(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.SELECT = true;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keydown_START(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.START = true;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keydown_RIGHT(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.RIGHT = true;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keydown_LEFT(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.LEFT = true;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keydown_DOWN(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.DOWN = true;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keydown_UP(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.UP = true;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keyup_A(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.A = false;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keyup_B(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.B = false;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keyup_SELECT(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.SELECT = false;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keyup_START(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.START = false;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keyup_RIGHT(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.RIGHT = false;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keyup_LEFT(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.LEFT = false;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keyup_DOWN(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.DOWN = false;
+    }
+
+    #[allow(non_snake_case)]
+    #[wasm_bindgen]
+    pub fn keyup_UP(&mut self) {
+        self.nes_state.borrow_mut().joypad.state_1p.UP = false;
     }
 }
